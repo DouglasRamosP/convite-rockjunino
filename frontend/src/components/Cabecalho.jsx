f1563c9c608edf8fe6245fa137f6de4be0fe65b6
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-06-02 22:40:33 -0300

    Corrige caminho da logo para compatibilidade com Vercel

--- frontend/src/components/Cabecalho.jsx
+++ frontend/src/components/Cabecalho.jsx
@@ -2,7 +2,7 @@ export default function Cabecalho() {
   return (
     <div className="flex flex-col items-center w-fit h-fit">
       <img
-        src="/img/logo-semFundo.png"
+        src="/fotos/logo-semFundo.png"
         alt="Rock Junino"
         className="block w-72 mx-auto -mb-12"
       />
